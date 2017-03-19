e86d59c765eea72fa148f47e1bb0b4ea1d923cc8
Michael Aldridge <aldridge.mac@gmail.com>
2017-03-19 02:23:16 -0500

    Whitespace cleanup in login.js

--- login.js
+++ login.js
@@ -2,12 +2,15 @@ var login = (function (lightdm, $) {
     // Initialize some variables
     var username = null;
     var password = null;
+
     // Initialize the timer
     password_interval_id = 0;
+
     // Grab the fields
     var $user = $('#user');
     var $pass = $('#pass');
     var $sess = $('#session');
+
     // List the sessions
     var setup_session_list = function () {
         var $list = $sess;
@@ -34,58 +37,43 @@ var login = (function (lightdm, $) {
                 );
             }
         });
-
-
     };
 
     // Authenticate
     var auth = function() {
         lightdm.cancel_timed_login();
-
-
         window.start_authentication();
-
     }
 
     var updateSession = function() {
-
 	//So, we *could* start a ldm auth session here, now that a username has been selected.
 	//This seems incredibly wasteful though, because you might type in a username and never click submit
-
 	//instead, we just grab the session (if there is one) and update the select box
 	var username = $user.val() || null;
         var session = lightdm_default_session;
         $('#session').val(session);
-
     }
 
     // Override the authentication so it grabs the username
     window.start_authentication = function () {
         username = $user.val() || null;
-
-
         lightdm.start_authentication(username);
     };
 
     // Override the provide_secret so it grabs the password
     window.provide_secret = function () {
-
         password = $pass.val() || null;
-        //show_prompt(password);
         if(password !== null) {
             lightdm.provide_secret(password);
         }
     };
 
-    // Remove this. It's useless. I swear.
     window.timed_login = function(user) {}
 
     // Set the session properly and then start it
     window.authentication_complete = function () {
-
         if (lightdm.is_authenticated) {
             show_prompt('Logged in');
-
             session_list = document.getElementById('session');
             session = lightdm.sessions[session_list.selectedIndex];
             lightdm.login(lightdm.authentication_user, session.key);
@@ -100,6 +88,7 @@ var login = (function (lightdm, $) {
         console.log('Error: ' + e);
 
     };
+
     //Lightdm prompts for a password...this is how we know we can send the secret
     window.show_prompt = function (e) {
         if(password_interval_id > 0) clearInterval(password_interval_id);
@@ -107,15 +96,14 @@ var login = (function (lightdm, $) {
             window.provide_secret();} , 250);
     };
 
-
     var init = function () {
         $(function () {
             setup_session_list();
             $('#error').hide();
 
-        $('#user').on('blur', function () {
-        updateSession();
-        });
+            $('#user').on('blur', function () {
+                updateSession();
+            });
 
             // Bind auth to the form
             $('form').on('submit', function (e) {
@@ -124,10 +112,10 @@ var login = (function (lightdm, $) {
             });
         });
     };
-
     return {
         init: init
     };
 } (lightdm, jQuery));
+
 //Start the task
 login.init();
